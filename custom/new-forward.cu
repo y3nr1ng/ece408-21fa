a75131ed82c02e1f88be397b9732c7aeae156afc
Liu, Yen-Ting <windows.linux.mac@gmail.com>
2021-11-24 03:30:44 -0600

    Get tiled shared memory convolution to work

--- custom/new-forward.cu
+++ custom/new-forward.cu
@@ -35,8 +35,7 @@ __global__ void conv_forward_kernel(float* y,
                                     const int H,
                                     const int W,
                                     const int K) {
-  __shared__ float
-      tile[PADDED_TILE_WIDTH * PADDED_TILE_WIDTH * PADDED_TILE_WIDTH];
+  __shared__ float tile[C_MAX * PADDED_TILE_WIDTH * PADDED_TILE_WIDTH];
 
   /*
   Function paramter definitions:
@@ -54,7 +53,7 @@ __global__ void conv_forward_kernel(float* y,
   const int H_out = H - K + 1;
   const int W_out = W - K + 1;
 
-#define y3d(i1, i0) \
+#define y2d(i1, i0) \
   y[b * (M * H_out * W_out) + m * (H_out * W_out) + (i1) * (W_out) + i0]
 #define t3d(i2, i1, i0)                                 \
   tile[(i2) * (PADDED_TILE_WIDTH * PADDED_TILE_WIDTH) + \
@@ -63,80 +62,52 @@ __global__ void conv_forward_kernel(float* y,
 #define k3d(i2, i1, i0) \
   kernel[m * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-  int tmp;
-
   // some alias for block/thread index
-  int bx = blockIdx.x, by = blockIdx.y, bz = blockIdx.z;
-  int tx = threadIdx.x, ty = threadIdx.y, tz = threadIdx.z;
+  int bx = blockIdx.x, by = blockIdx.y;
+  int tx = threadIdx.x, ty = threadIdx.y;
 
-  // destination linear index
-  int dst = (tz * TILE_WIDTH * TILE_WIDTH) + (ty * TILE_WIDTH) + tx;
-  // 3D index inside a padded tiles
-  tmp = dst;
-  int dst_x = tmp % PADDED_TILE_WIDTH;
-  tmp /= PADDED_TILE_WIDTH;
-  int dst_y = tmp % PADDED_TILE_WIDTH;
-  tmp /= PADDED_TILE_WIDTH;
-  int dst_z = tmp;
-  // 3D index in global array, simply subtract the pad size
-  int src_x = (bx * TILE_WIDTH + dst_x) - KERNEL_RADIUS;
-  int src_y = (by * TILE_WIDTH + dst_y) - KERNEL_RADIUS;
-  int src_z = (bz * TILE_WIDTH + dst_z) - KERNEL_RADIUS;
+  int dst_x, dst_y, src_x, src_y;
 
-  // load 1, this include "left halos" and "content"
-  if (((src_x >= 0) && (src_x < W)) && ((src_y >= 0) && (src_y < H)) &&
-      ((src_z >= 0) && (src_z < C))) {
-    t3d(dst_z, dst_y, dst_x) = x3d(src_z, src_y, src_x);
-  } else {
-    t3d(dst_z, dst_y, dst_x) = 0.0f;
-  }
+  for (int c = 0; c < C; c++) {
+    for (int dst = ty * TILE_WIDTH + tx;
+         dst < PADDED_TILE_WIDTH * PADDED_TILE_WIDTH;
+         dst += TILE_WIDTH * TILE_WIDTH) {
+      // 3D index inside a padded tiles
+      dst_x = dst % PADDED_TILE_WIDTH;
+      dst_y = dst / PADDED_TILE_WIDTH;
+      // 3D index in global array, simply subtract the pad size
+      src_x = (bx * TILE_WIDTH + dst_x);
+      src_y = (by * TILE_WIDTH + dst_y);
 
-  // load 2, "right halos",
-  dst = (tz * TILE_WIDTH * TILE_WIDTH) + (ty * TILE_WIDTH) + tx +
-        (TILE_WIDTH * TILE_WIDTH * TILE_WIDTH);
-  tmp = dst;
-  dst_x = tmp % PADDED_TILE_WIDTH;
-  tmp /= PADDED_TILE_WIDTH;
-  dst_y = tmp % PADDED_TILE_WIDTH;
-  tmp /= PADDED_TILE_WIDTH;
-  dst_z = tmp;
-  src_x = (bx * TILE_WIDTH + dst_x) - KERNEL_RADIUS;
-  src_y = (by * TILE_WIDTH + dst_y) - KERNEL_RADIUS;
-  src_z = (bz * TILE_WIDTH + dst_z) - KERNEL_RADIUS;
-  if (dst_z < PADDED_TILE_WIDTH) {
-    if (((src_x >= 0) && (src_x < W)) &&
-        ((src_y >= 0) && (src_y < H)) && ((src_z >= 0) && (src_z < C))) {
-      t3d(dst_z, dst_y, dst_x) = x3d(src_z, src_y, src_x);
-    } else {
-      t3d(dst_z, dst_y, dst_x) = 0.0f;
+      if ((src_x < W) && (src_y < H)) {
+        t3d(c, dst_y, dst_x) = x3d(c, src_y, src_x);
+      } else {
+        t3d(c, dst_y, dst_x) = 0.0f;
+      }
     }
   }
 
   __syncthreads();
 
-  // update the destination 3D index
-  dst_x = bx * TILE_WIDTH + tx;
-  dst_y = by * TILE_WIDTH + ty;
-  dst_z = bz * TILE_WIDTH + tz;
-
-  if (dst_z == 0) {
-    // the actual convolution
-    float sum = 0;
-    for (int c = 0; c < C; c++) {
-      for (int p = 0; p < K; p++) {
-        for (int q = 0; q < K; q++) {
-          sum += t3d(tz + c, ty + p, tx + q) * k3d(c, p, q);
-        }
+  // the actual convolution
+  float sum = 0;
+  for (int c = 0; c < C; c++) {
+    for (int p = 0; p < K; p++) {
+      for (int q = 0; q < K; q++) {
+        sum += t3d(c, ty + p, tx + q) * k3d(c, p, q);
       }
-    }
-
-    // restore the linear index in global scope
-    if ((dst_x < W_out) && (dst_y < H_out)) {
-      y3d(dst_y, dst_x) = sum;
     }
   }
 
-#undef y3d
+  // update the destination 3D index
+  dst_x = bx * TILE_WIDTH + tx;
+  dst_y = by * TILE_WIDTH + ty;
+  // restore the linear index in global scope
+  if ((dst_x < W_out) && (dst_y < H_out)) {
+    y2d(dst_y, dst_x) = sum;
+  }
+
+#undef y2d
 #undef t3d
 #undef x3d
 #undef k3d
@@ -162,6 +133,8 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float* host_y,
   const size_t bytes_x = (B * C * H * W) * sizeof(float);
   const size_t bytes_k = (M * C * K * K) * sizeof(float);
 
+  printf("(B=%d, M=%d, C=%d, H=%d, W=%d, K=%d)\n", B, M, C, H, W, K);
+
   // Allocate memory
   cudaErrChk(cudaMalloc(device_y_ptr, bytes_y));
   cudaErrChk(cudaMalloc(device_x_ptr, bytes_x));
@@ -185,13 +158,11 @@ __host__ void GPUInterface::conv_forward_gpu(float* device_y,
   const int H_out = H - K + 1;
   const int W_out = W - K + 1;
 
-  dim3 block(TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-  dim3 grid(ceil((float)H_out / TILE_WIDTH), ceil((float)W_out / TILE_WIDTH),
-            ceil((float)C / TILE_WIDTH));
+  dim3 block(TILE_WIDTH, TILE_WIDTH);
+  dim3 grid(ceil((float)H_out / TILE_WIDTH), ceil((float)W_out / TILE_WIDTH));
 
-  std::cout << "grid=(" << grid.x << ", " << grid.y << ", " << grid.z << "), "
-            << "block=(" << block.x << ", " << block.y << ", " << block.z
-            << ") " << std::endl;
+  printf("grid=(x=%d, y=%d, z=%d), block=(x=%d, y=%d, z=%d)\n", grid.x, grid.y,
+         grid.z, block.x, block.y, block.z);
 
   // Call the kernel
   for (int b = 0; b < B; b++) {
